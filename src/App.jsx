8dc181666faaf246a7eb3f2a0c939759cdd98edf
Prince <princekumar7320918928@gmail.com>
2024-10-21 11:29:06 +0530

    added header and footer

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div className='min-h-screen'>
+      <Navbar></Navbar>
+      <div className='h-[100vh] w-[90%] mx-auto'>
+        
+      </div>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
