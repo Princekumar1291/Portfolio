2af82e78f88af4736dad15c00223cacb807510dc
Prince <princekumar7320918928@gmail.com>
2024-10-22 00:47:09 +0530

    upd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600" style={{ opacity: 0.9 }}>
+    <nav className="bg-white dark:bg-gray-900 fixed w-full z-[100] top-0 start-0 border-b border-gray-200 dark:border-gray-600" style={{ opacity: 0.9 }}>
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <div className="logo-container" style={{ perspective: '1000px' }}>
